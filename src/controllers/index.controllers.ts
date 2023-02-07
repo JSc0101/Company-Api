79ef7ab39703c8c5adcf46b3f790142875e3dc3e
Sebastian009w <ia3266505@gmail.com>
2023-02-07 18:45:05 -0500

    Buscar user por id

--- src/controllers/index.controllers.ts
+++ src/controllers/index.controllers.ts
@@ -1,18 +1,25 @@
-import { query, Request, Response } from "express";
+import { Request, Response } from "express";
 import { pool } from "../database/DB";
 
-export const getCompany = async (req: Request, res: Response) => {
+export const getCompany = async (req: Request, res: Response):Promise<Response> => {
   const { rows } = await pool.query(`SELECT * FROM employees;`);
-  res.json(rows);
+  return res.json(rows);
 };
 
-export const saveCompany = async (req: Request, res: Response) => {
+export const saveCompany = async (req: Request, res: Response):Promise<Response> => {
   const { name, lastname, age, rol, photo, description, email } = req.body;
   const consult = `INSERT INTO employees(name,lastname,age, rol, photo, description, email) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
   const values = [name, lastname, age, rol, photo, description, email];
   const response = await pool.query(consult, values);
   console.log(response);
-  res.json({
+  return res.json({
     "save succesfuly": "User saving succesfuly",
   });
+};
+
+export const userById = async (req: Request, res: Response):Promise<Response> => {
+  const id = req.params.id;
+  const consult = `SELECT * FROM employees WHERE id = $1`;
+  const { rows } = await pool.query(consult, [id]);
+  return res.json(rows);
 };
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { getCompany, saveCompany } from "../controllers/index.controllers";
+import { getCompany, saveCompany,userById } from "../controllers/index.controllers";
 const router = Router();
 router.get("/", getCompany);
+router.get("/:id", userById);
 router.post("/", saveCompany);
 export default router;
