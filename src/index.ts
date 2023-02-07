50297ac2a6f88fb1554c3cd0194f0c810ceb0339
Sebastian009w <ia3266505@gmail.com>
2023-02-06 21:34:08 -0500

    Inicializacion del servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Rest api Company')
+import express from "express";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"));
+console.log(`Listen in the port ${app.get("port")}`);
