98896378218797d155c567ea4d71b3b457f092a9
Sebastian009w <ia3266505@gmail.com>
2023-02-08 22:14:20 -0500

    Manejo de errores

--- src/controllers/index.controllers.ts
+++ src/controllers/index.controllers.ts
@@ -1,61 +1,76 @@
 import { Request, Response } from "express";
 import { pool } from "../database/DB";
 
-export const getCompany = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { rows } = await pool.query(`SELECT * FROM employees;`);
-  return res.json(rows);
+export const getCompany = async (req: Request, res: Response) => {
+  try {
+    const { rows } = await pool.query(`SELECT * FROM employees;`);
+    return res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({
+      error: "Something goes wrong",
+    });
+  }
 };
 
-export const saveCompany = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { name, lastname, age, rol, photo, description, email } = req.body;
-  const consult = `INSERT INTO employees(name,lastname,age, rol, photo, description, email) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
-  const values = [name, lastname, age, rol, photo, description, email];
-  const response = await pool.query(consult, values);
-  console.log(response);
-  return res.json({
-    "save succesfuly": "User saving succesfuly",
-  });
+export const saveCompany = async (req: Request, res: Response) => {
+  try {
+    const { name, lastname, age, rol, photo, description, email } = req.body;
+    const consult = `INSERT INTO employees(name,lastname,age, rol, photo, description, email) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
+    const values = [name, lastname, age, rol, photo, description, email];
+    await pool.query(consult, values);
+
+    res.status(200).json({
+      "save succesfuly": "User saving succesfuly",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Something goes wrong",
+    });
+  }
 };
 
-export const userById = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const id = req.params.id;
-  const consult = `SELECT * FROM employees WHERE id = $1`;
-  const { rows } = await pool.query(consult, [id]);
-  return res.json(rows);
+export const userById = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const consult = `SELECT * FROM employees WHERE id = $1`;
+    const { rows } = await pool.query(consult, [id]);
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({
+      error: "Something goes wrong",
+    });
+  }
 };
 
-export const updateUser = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { name, lastname, age, rol, photo, description, email } = req.body;
-  const id = req.params.id;
-  const consult = `UPDATE employees SET name = $1, lastname = $2, age = $3 , rol = $4 , photo = $5 , description = $6, email = $7 WHERE id = $8`;
-  const values = [name, lastname, age, rol, photo, description, email, id];
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { name, lastname, age, rol, photo, description, email } = req.body;
+    const id = req.params.id;
+    const consult = `UPDATE employees SET name = $1, lastname = $2, age = $3 , rol = $4 , photo = $5 , description = $6, email = $7 WHERE id = $8`;
+    const values = [name, lastname, age, rol, photo, description, email, id];
 
-  await pool.query(consult, values);
+    await pool.query(consult, values);
 
-  return res.json({
-    "user update succesfuly": "update",
-  });
+    res.json({
+      "user update succesfuly": "update",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Something goes wrong",
+    });
+  }
 };
 
-export const deleteUser = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const id = req.params.id;
-  await pool.query("DELETE FROM employees WHERE id = $1", [id]);
-  return res.json({
-    user: "succesfuly delete",
-  });
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    await pool.query("DELETE FROM employees WHERE id = $1", [id]);
+    return res.json({
+      user: "succesfuly delete",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Something goes wrong",
+    });
+  }
 };
