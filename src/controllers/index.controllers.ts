6454c5988037837f951ff4668dbc3e8f09412bb2
Sebastian009w <ia3266505@gmail.com>
2023-02-08 21:45:37 -0500

    Se actuliza el usuario

--- src/controllers/index.controllers.ts
+++ src/controllers/index.controllers.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
 import { pool } from "../database/DB";
 
-export const getCompany = async (req: Request, res: Response):Promise<Response> => {
+export const getCompany = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
   const { rows } = await pool.query(`SELECT * FROM employees;`);
   return res.json(rows);
 };
 
-export const saveCompany = async (req: Request, res: Response):Promise<Response> => {
+export const saveCompany = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
   const { name, lastname, age, rol, photo, description, email } = req.body;
   const consult = `INSERT INTO employees(name,lastname,age, rol, photo, description, email) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
   const values = [name, lastname, age, rol, photo, description, email];
@@ -17,9 +23,28 @@ export const saveCompany = async (req: Request, res: Response):Promise<Response>
   });
 };
 
-export const userById = async (req: Request, res: Response):Promise<Response> => {
+export const userById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
   const id = req.params.id;
   const consult = `SELECT * FROM employees WHERE id = $1`;
   const { rows } = await pool.query(consult, [id]);
   return res.json(rows);
+};
+
+export const updateUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { name, lastname, age, rol, photo, description, email } = req.body;
+  const id = req.params.id;
+  const consult = `UPDATE employees SET name = $1, lastname = $2, age = $3 , rol = $4 , photo = $5 , description = $6, email = $7 WHERE id = $8`;
+  const values = [name, lastname, age, rol, photo, description, email, id];
+
+  await pool.query(consult, values);
+
+  return res.json({
+    "user update succesfuly": "update",
+  });
 };
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { getCompany, saveCompany,userById } from "../controllers/index.controllers";
+import {
+  getCompany,
+  saveCompany,
+  userById,
+  updateUser,
+} from "../controllers/index.controllers";
 const router = Router();
 router.get("/", getCompany);
 router.get("/:id", userById);
 router.post("/", saveCompany);
+router.put("/:id", updateUser);
 export default router;
